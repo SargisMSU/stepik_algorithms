c8755834bd9a5a7e9d176d285d7761ee912ac134
SargisMSU <sargis.abrahamyan.1998@mail.ru>
2022-03-09 19:03:40 +0400

    + merge sort algorithm

---
+++ src/main/java/divide_rule/MergeSort.java
@@ -0,0 +1,58 @@
+package divide_rule;
+
+import java.util.Scanner;
+
+public class MergeSort {
+
+    private static long count = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] A = new int[length];
+        for (int i = 0; i < length; i++) {
+            A[i] = scanner.nextInt();
+        }
+        mergeSort(A, length);
+        System.out.println(count);
+    }
+
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                count += left - i;
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+}
