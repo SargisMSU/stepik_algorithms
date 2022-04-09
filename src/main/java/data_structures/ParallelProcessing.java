865fd7d67a2517e622a2fc6607cb81411a13f2c7
SargisMSU <sargis.abrahamyan.1998@mail.ru>
2022-04-09 13:03:08 +0400

    + ParallelProcessing problem solution

---
+++ src/main/java/data_structures/ParallelProcessing.java
@@ -0,0 +1,63 @@
+package data_structures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
+public class ParallelProcessing {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        long[] firstLine = Arrays.stream(reader.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long[] tasks = Arrays.stream(reader.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long processorsCount = firstLine[0];
+        long tasksCount = firstLine[1];
+        PriorityQueue<Entry> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            int timeDiff = (int) (o1.getTime() - o2.getTime());
+            if (timeDiff != 0)
+                return timeDiff;
+            else return (int) (o1.getProcessor() - o2.getProcessor());
+        });
+
+        for (int i = 0; i < processorsCount; i++) {
+            priorityQueue.add(new Entry(i, 0));
+        }
+
+        for (long task : tasks) {
+            Entry entry = priorityQueue.poll();
+            System.out.println(entry.getProcessor() + " " + entry.getTime());
+            entry.setTime(entry.getTime() + task);
+            priorityQueue.add(entry);
+        }
+    }
+}
+
+
+class Entry {
+    private long processor, time;
+
+    public Entry(long processor, long time) {
+        this.processor = processor;
+        this.time = time;
+    }
+
+    public long getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(long processor) {
+        this.processor = processor;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
